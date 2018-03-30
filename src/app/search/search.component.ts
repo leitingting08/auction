f5908faffca0e6b048f9435fd2158af3d682ae27
leitingting08 <leitingting08@gmail.com>
2018-03-30 13:39:22 +0800

    check_ep

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,4 +12,22 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
   }
 
+private departs = [
+    {name:'全选'},
+    {name:'张三'},
+    {name:'李四'},
+    {name:'王五'}
+  ]
+
+ //全选
+  checked(m){
+      this.checkAll(m,m.checked[0]);
+　 }
+
+  checkAll(dir,boolean){
+     dir.checked=[];
+     dir.forEach(i=>{
+     	dir.checked.push(boolean);
+     })
+  }
 }
