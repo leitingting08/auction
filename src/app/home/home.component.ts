f3077ddf6fc35fff407d16ab5a20b0216f6f1113
leitingting08 <leitingting08@gmail.com>
2019-03-07 17:12:04 +0800

    checkedAll_demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  departs = [
+    {'name':'全选',checked:false},
+    {'name':'张三',checked:false},
+    {'name':'李四',checked:false},
+    {'name':'王五',checked:false}
+  ]
   ngOnInit() {
+  }
+
+  checkEvent(item,i){
+  	
+  	if(i===0){
+  		console.log(0)
+  		if(item.checked){
+  			this.departs.forEach(m=>{m.checked = true})
+  		}else{
+  			this.departs.forEach(m=>{m.checked = false})
+  		}
+  	}else{
+  		let res = this.departs.some(m=>{return !m.checked});
+  		if(res){ // 如果全选以后，其中一个取消选择
+  			this.departs[0].checked = false;
+  		}
+  		let flag = true;
+  		for(var n=1;n<this.departs.length;n++){
+  			if(!this.departs[n].checked){
+  				flag = false;
+  			}
+  		}
+  		if(flag){  // 如果全选以后，其他的全部选中了，就把全选按钮也选中
+  			this.departs[0].checked = true;
+  		}
+  	}
+  	console.log(this.departs[i])
   }
 
 }
